63765ca69117ec4c09b5256349fd062bd561bd5c
kantoglu <mertkantoglu1@gmail.com>
2025-03-04 00:13:41 +0300

    ödev 3 eklendi.

---
+++ odev3_mert/model/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    surname: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    email: {
+      type: Schema.Types.String,
+      required: true,
+      unique: true,
+    },
+    password: { type: Schema.Types.String, required: true },
+  },
+  {
+    timestamps: true,
+    autoIndex: true,
+    minimize: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema, "user");
+
+module.exports = User;
---
+++ odev3_mert/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const config = require("./config/server.config");
+const db = require("./db/db");
+const router = require("./router/router");
+const app = express();
+app.use(express.json());
+config.initialServerConfig();
+
+const PORT = process.env.PORT || 3000;
+app.use(router)
+db.dbConnect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server ${PORT} portunda çalışıyor`);
+    });
+  })
+  .catch((e) => {
+    console.log(e.message);
+  });
