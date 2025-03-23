d02806da153fc308ae842dd4006e224138d83730
Dilan <dilanderegizu@icloud.com>
2025-03-23 19:53:54 +0300

    Update controller.js

--- ödev4_dilan/controller/controller.js
+++ ödev4_dilan/controller/controller.js
@@ -34,85 +34,120 @@ exports.login = async (req, res) => {
 };
 
 exports.createUser = async (req, res) => {
-    try {
-        const { name, surname, email, password } = req.body;
-        const user = new User({ name, surname, email, password: md5(password) });
-        await user.save();
-
-        res.status(201).json({ error: false, success: true, message: "Kayıt başarılı", data: user });
-    } catch (error) {
-        res.status(500).json({ error: true, success: false, message: error.message });
-    }
+  try {
+    const { name, surname, email, password } = req.body;
+    const _password = md5(password);
+    const user = new User({
+      name,
+      surname,
+      password: _password,
+      email,
+    });
+    await user.save();
+    res.status(201).json({
+      error: false,
+      success: true,
+      message: "Kayıt başarılı",
+      data: user,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, success: false, message: error.message });
+  }
 };
 
 exports.updateUser = async (req, res) => {
-    try {
-        const { userId } = req.params;
-        const { name, surname } = req.body;
-        const updatedUser = await User.findByIdAndUpdate(userId, { name, surname }, { new: true });
-        
-        if (!updatedUser) {
-            return res.status(404).json({ error: true, success: false, message: "Kullanıcı bulunamadı" });
-        }
-
-        res.status(200).json({ error: false, success: true, message: "Güncelleme başarılı", data: updatedUser });
-    } catch (error) {
-        res.status(500).json({ error: true, success: false, message: error.message });
-    }
+  try {
+    const { userId } = req.params;
+    const { name, surname } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        name: name,
+        surname: surname,
+      },
+      { new: true }
+    );
+    res.status(200).json({
+      error: false,
+      success: true,
+      message: "Güncelleme başarılı",
+      data: updatedUser,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, success: false, message: error.message });
+  }
 };
-
 exports.getUserById = async (req, res) => {
-    try {
-        const { userId } = req.params;
-        const user = await User.findById(userId);
-        
-        if (!user) {
-            return res.status(404).json({ error: true, success: false, message: "Kullanıcı bulunamadı" });
-        }
-
-        res.status(200).json({ error: false, success: true, message: "Kullanıcı listelendi", data: user });
-    } catch (error) {
-        res.status(500).json({ error: true, success: false, message: error.message });
-    }
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    res
+      .json({
+        error: false,
+        success: true,
+        message: "Kullanıcı listelendi",
+        data: user,
+      })
+      .status(200);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, success: false, message: error.message });
+  }
 };
-
 exports.getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find();
-        res.status(200).json({ error: false, success: true, message: "Kullanıcılar listelendi", data: users });
-    } catch (error) {
-        res.status(500).json({ error: true, success: false, message: error.message });
-    }
+  try {
+    const users = await User.find();
+    res
+      .json({
+        error: false,
+        success: true,
+        message: "Kullanıcılar listelendi",
+        data: users,
+      })
+      .status(200);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, success: false, message: error.message });
+  }
 };
-
 exports.deleteUserById = async (req, res) => {
-    try {
-        const { userId } = req.params;
-        const user = await User.findByIdAndDelete(userId);
-        
-        if (!user) {
-            return res.status(404).json({ error: true, success: false, message: "Kullanıcı bulunamadı" });
-        }
-
-        res.status(200).json({ error: false, success: true, message: "Kullanıcı silindi" });
-    } catch (error) {
-        res.status(500).json({ error: true, success: false, message: error.message });
-    }
+  try {
+    const { userId } = req.params;
+    await User.findByIdAndDelete(userId);
+    res
+      .status(200)
+      .json({ error: false, success: true, messgae: "Kullanıcı silindi" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, success: false, message: error.message });
+  }
 };
-
 exports.updatePassword = async (req, res) => {
-    try {
-        const { userId } = req.params;
-        const { newPassword } = req.body;
-        const updatedUser = await User.findByIdAndUpdate(userId, { password: md5(newPassword) }, { new: true });
-        
-        if (!updatedUser) {
-            return res.status(404).json({ error: true, success: false, message: "Kullanıcı bulunamadı" });
-        }
-
-        res.status(200).json({ error: false, success: true, message: "Şifre güncellendi", data: updatedUser });
-    } catch (error) {
-        res.status(500).json({ error: true, success: false, message: error.message });
-    }
-};
-
+  try {
+    const { userId } = req.params;
+    const { newPassword } = req.body;
+    const _password = md5(newPassword);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { password: _password },
+      { new: true }
+    );
+    res.status(200).json({
+      error: false,
+      success: true,
+      message: "Güncelleme başarılı",
+      data: updatedUser,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: true, success: false, message: error.message });
+  }
+}; 
