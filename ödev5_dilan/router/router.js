1ff11278e9fbaf7ee472a900c024944f7c06f06c
Dilan <dilanderegizu@icloud.com>
2025-03-24 02:48:28 +0300

    Update router.js
    eksik router düzenlendi

--- ödev5_dilan/router/router.js
+++ ödev5_dilan/router/router.js
@@ -4,4 +4,6 @@ const controller = require("../controller/user.controller");
 router.post("/register", controller.register);
 router.post("/not-hesapla",controller.notHesapla)
 
+router.post("/burc-hesapla/:userId",controller.burcHesapla)
+
 module.exports = router;
