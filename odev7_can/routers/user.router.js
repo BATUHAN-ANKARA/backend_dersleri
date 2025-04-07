f77fbb251c028496d58cc3cfcb14efdabb484618
Ahmet Can <can_tanrikulu@icloud.com>
2025-04-07 15:17:12 +0300

    odev7_can added

---
+++ odev7_can/routers/user.router.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const controller = require("../controllers/user.controller");
+
+router.post("/register", controller.register);
+router.post("/login", controller.login);
+router.put("/updateUser/:userId", controller.updateUser);
+router.delete("/deleteUser/:userId", controller.deleteUser);
+router.get("/getAllUsers", controller.getAllUsers);
+
+router.get("/sorgu1", controller.sorgu1);
+router.get("/sorgu2", controller.sorgu2);
+router.get("/sorgu3", controller.sorgu3);
+router.get("/sorgu4", controller.sorgu4);
+
+
+module.exports = {
+  user: router,
+};
---
+++ odev7_can/services/user.service.js
@@ -0,0 +1,153 @@
+const User = require("../models/user.model");
+const utils = require("../utils/index");
+
+exports.register = async (req) => {
+  try {
+    const { name, surname, email, password, birthDate, gender } = req.body;
+    const existUser = await User.findOne({ email: email });
+    if (existUser) {
+      throw new Error("Email zaten kullanımda");
+    }
+    const _password = utils.helper.hashToPassword(password);
+    const age = utils.helper.yasHesap(birthDate);
+    const zodiac = utils.helper.burcHesap(day,month);
+    const user = new User({
+      name,
+      surname,
+      email,
+      password: _password,
+      birthDate,
+      age,
+      zodiac,
+      gender,
+    });
+
+    await user.save();
+    
+    return user;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.login = async (req) => {
+  try {
+    const { email, password } = req.body;
+    const _password = utils.helper.hashToPassword(password);
+    const user = await User.findOne({ email: email, password: _password });
+    if (!user) {
+      throw new Error("Kullanıcı bilgileri yanlış");
+    }
+    return user;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.updateUser = async (req) => {
+  try {
+    const { userId } = req.params;
+    const { name, surname } = req.body;
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error("Kullanıcı bulunamadı");
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { name: name, surname: surname },
+      { new: true }
+    );
+    return updatedUser;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.deleteUser = async (req) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error("Kullanıcı bulunamadı");
+    }
+    await User.findByIdAndDelete(userId);
+    return "Kullanıcı başarılı şekilde silindi";
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.getAllUsers = async () => {
+  try {
+    const users = await User.find();
+    return users;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.sorgu1 = async () => {
+  try {
+    const users = await User.find({
+      gender: { $exists: true, $ne: null },
+      zodiac: { $exists: true, $ne: null },
+      age: { $exists: false }
+    });
+    return users;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.sorgu2 = async () => {
+  try {
+    const users = await User.find({
+      gender: "Kadın",
+      zodiac: { $exists: true, $ne: null },
+      age: { $gte: 18, $lte: 40 }
+    });
+    return users;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.sorgu3 = async () => {
+  try {
+    const users = await User.find({
+      $or: [
+        { gender: { $exists: false } },
+        { birthDate: { $exists: false } },
+        { age: { $exists: false } }
+      ]
+    });
+    return users;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.sorgu4 = async () => {
+  try {
+    const users = await User.find({
+      $or: [
+        { gender: "Erkek" },
+        { zodiac: { $exists: true, $ne: null } }
+      ]
+    });
+    return users;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+
+
+/*
+sorgu1- cinsiyeti gender* olan, burcu zodiac* olan ve age bilgisi bulunmayan kullanıcılar
+sorgu2- cinsiyeti Kadın olan, burcu zodiac* olan age aralığı 18-40 olan kullanıcılar
+sorgu3- cinsiyeti bulunmayan veya doğum tarihi bulunmayan veya yaş bilgisi bulunmayan kullanıcılar
+sorgu4- cinsiyeti Erkek olan veya burcu zodiac* olan (or)
+*/
+
+
