b793e93a1fa7fe349f9784af8168e17624864de4
Dilan <dilanderegizu@icloud.com>
2025-04-28 15:16:40 +0300

    Update server.js

--- dilan_ödev2/server.js
+++ dilan_ödev2/server.js
@@ -1,19 +1,18 @@
 const express = require("express");
-<<<<<<< HEAD
+
 const router = require("./router/router");
 const db = require("./db/db");
-=======
+
 const db = require("./db/db");
 const router = require("./router/router");
->>>>>>> 85d947e728a09e885b0882661940307917787dda
+
 const app = express();
 
 app.use(express.json());
 app.use(router);
-<<<<<<< HEAD
-=======
 
->>>>>>> 85d947e728a09e885b0882661940307917787dda
+
+
 db.mongooseConnection()
   .then(() => {
     app.listen(3000, () => {
@@ -21,9 +20,9 @@ db.mongooseConnection()
     });
   })
   .catch((e) => {
-<<<<<<< HEAD
+
     console.log("Bağlantı hatası", e.message);
-=======
+
     console.log("Bağlantı hatası:", e.message);
->>>>>>> 85d947e728a09e885b0882661940307917787dda
+
   });
